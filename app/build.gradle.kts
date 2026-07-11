1cd362e1c5ba5eab92182cef7716499484bfe35f
James/Splinch <jf358950@gmail.com>
2026-07-11 15:55:04 +0000

    build: allow debug build without google-services plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
-    id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
 }
 
